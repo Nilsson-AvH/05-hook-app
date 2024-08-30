4ac4ddeec7e70de7317d288e3b2967abad46345e
Nilsson-AvH <nlopez153@misena.edu.co>
2024-08-29 21:46:08 -0500

    Second commit

---
+++ src/01-useState/CounterApp.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+import { useState } from "react"
+
+export const CounterApp = () => {
+
+    const [counter, setCounter] = useState(10)
+
+  return (
+    <>
+        <h1>Counter: {counter} </h1>
+      
+        <hr />
+
+        <button className="btn" onClick={ () => setCounter( counter+1 ) } >+1</button>
+        <button className="btn" onClick={ () => setCounter( counter-1 ) } >-1</button>        
+    </>
+
+  )
+}
